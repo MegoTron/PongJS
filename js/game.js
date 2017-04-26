08148e143cf1920e95ed7150273832480218dfc4
llister15 <llister@wonkasoft.com>
2017-04-26 00:10:41 -0700

    added sound and fix scoreboard

--- js/game.js
+++ js/game.js
@@ -1,12 +1,20 @@
-var WIDTH=1200, HEIGHT=1000, pi=Math.PI;
+var WIDTH=1000, HEIGHT=1000, pi=Math.PI;
    var UpArrow = 38, DownArrow = 40;
    var canvas, ctx, keystate;
    var player, ai, ball;
-   var pong1 = new Audio('audio/pong1.ogg');
-   var pong2 = new Audio('audio/pong2.ogg');
-   var pong3 = new Audio('audio/pong3.ogg');
-  
+   var player1score,player2score;
+   var player1points=0,player2points=0;
+   var music = new Audio('./audio/music.ogg');
+   var levelup = new Audio('./audio/levelup.ogg');
+   var leveldown = new Audio('./audio/leveldown.ogg');
+   var pong2 = new Audio('./audio/pong2.ogg');
+   var pong3 = new Audio('./audio/pong3.ogg');
+   var win1 = new Audio('./audio/player1w.ogg');
+   var win2 = new Audio('./audio/player2w.ogg');
+   music.play();
 
+  
+// Player 1 setup
    player = {
      x: null,
      y: null,
@@ -14,16 +22,46 @@ var WIDTH=1200, HEIGHT=1000, pi=Math.PI;
      height: 100,
 
      update: function() {
-       if (keystate[UpArrow]) this.y -= 7;
-       if (keystate[DownArrow]) this.y += 7;
+      // Player 1 speed
+       if (keystate[UpArrow]) this.y -= 10;
+       if (keystate[DownArrow]) this.y += 10;
 
 
      },
+     // Draw Player 1
      draw: function() {
        ctx.fillRect(this.x, this.y, this.width, this.height);
      }
    };
 
+   // Player 1 score setup
+   player1score = {
+     x: null,
+     y: null,
+
+     update: function() {
+      player1points++;
+
+      if (player1points == 5 || player2points == 5) {
+        if (player1points > player2points) {
+          win1.play();
+          alert("Player 1 is the Winner")
+        } else {
+          win2.play();
+          alert("Player 2 is the Winner");
+        }
+        location.reload();
+      }
+
+     },
+     // Draw Player 1
+     draw: function() {
+        ctx.font="90px atari full";
+        ctx.fillText(player1points,400,80);
+     }
+   };
+
+// Player 2 setup
    ai ={
      x: null,
      y: null,
@@ -39,15 +77,40 @@ var WIDTH=1200, HEIGHT=1000, pi=Math.PI;
      }
    };
 
+      // Player 2 score setup
+   player2score = {
+     x: null,
+     y: null,
+
+     update: function() {
+      player2points++;
+      if (player1points == 5 || player2points == 5) {
+        if (player1points > player2points) {
+          win1.play();
+          alert("Player 1 is the Winner")
+        } else {
+          win2.play();
+          alert("Player 2 is the Winner");
+        }
+        location.reload();
+      }
+
+     },
+     // Draw Player 2
+     draw: function() {
+        ctx.font="90px atari full";
+        ctx.fillText(player2points,550,80);
+     }
+   };
+
    ball ={
      x: null,
      y: null,
      vel: null,
      side: 20,
-     speed: 12,
+     speed: 13,
 
      serve: function(side) {
-       pong1.play();
 
        var r = Math.random();
        this.x = side===1 ? player.x : ai.x - this.side;
@@ -81,10 +144,9 @@ var WIDTH=1200, HEIGHT=1000, pi=Math.PI;
          this.x, this.y, this.side, this.side)
          ) {
         pong2.play();
-         this.x = pdle===player ? player.x+player.width : ai.x - this.side;
-       var n = (this.y+this.side - pdle.y) / (pdle.height+this.side);
-       var phi = 0.25*pi*(2*n - 1);
-
+        this.x = pdle===player ? player.x+player.width : ai.x - this.side;
+        var n = (this.y+this.side - pdle.y) / (pdle.height+this.side);
+        var phi = 0.25*pi*(2*n - 1);
        var smash = Math.abs(phi) > 0.2*pi ? 1.5 : 1;
        this.vel.x = smash*(pdle===player ? 1 : -1)*this.speed*Math.cos(phi);
        this.vel.y = smash*this.speed*Math.sin(phi);
@@ -92,6 +154,16 @@ var WIDTH=1200, HEIGHT=1000, pi=Math.PI;
 
      if (0 > this.x+this.side || this.x > WIDTH) {
          this.serve(pdle===player ? 1 : -1);
+        // When player 1 makes a point
+         if (this.x > 500 ) {
+          player1score.update();
+          levelup.play();
+        } 
+        // when player 2 makes a point
+        else {
+          player2score.update();
+          leveldown.play();
+        }
      }
  },
  draw: function() {
@@ -151,6 +223,8 @@ function draw() {
 
  ball.draw();
  player.draw();
+ player1score.draw();
+ player2score.draw();
  ai.draw();
 
  var w = 4;
